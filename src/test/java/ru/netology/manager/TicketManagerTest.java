47efe542dc40932b6dfde1d1f3769d599ec792f8
Nikita <trofimov1nv@gmail.com>
2020-12-24 09:46:18 +0300

    Edit TicketManagerTest, added repository in TicketManager

--- src/test/java/ru/netology/manager/TicketManagerTest.java
+++ src/test/java/ru/netology/manager/TicketManagerTest.java
@@ -3,13 +3,12 @@ package ru.netology.manager;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.Ticket;
 import ru.netology.repository.TicketRepository;
-import ru.netology.manager.TicketManager;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TicketManagerTest {
     private TicketRepository repository = new TicketRepository();
-    private TicketManager manager = new TicketManager();
+    private TicketManager manager = new TicketManager(repository);
 
     @Test
     void shouldAirportSearch() {
@@ -26,6 +25,24 @@ class TicketManagerTest {
         String from = "VLG";
 
         Ticket[] expected = new Ticket[]{third};
+        Ticket[] actual = manager.airportSearch(to, from);
+        assertArrayEquals(expected, actual);
+    }
+    @Test
+    void noTicketNeeded() {
+
+        Ticket first = new Ticket(1, 2000, "VLG", "MSK", 500);
+        Ticket second = new Ticket(1, 2000, "KZN", "MSK", 500);
+        Ticket third = new Ticket(1, 2000, "MSK", "KZN", 500);
+
+        repository.save(first);
+        repository.save(second);
+        repository.save(third);
+
+        String to = "KZN";
+        String from = "VLG";
+
+        Ticket[] expected = new Ticket[]{};
         Ticket[] actual = manager.airportSearch(to, from);
         assertArrayEquals(expected, actual);
     }
